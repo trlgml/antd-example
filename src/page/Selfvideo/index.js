f7effd994515c811545410770cbaaa555c764330
guomingliang <guomingliang@dwnews.com>
2021-01-14 13:53:54 +0800

    feat:video播放器调整

--- src/page/Selfvideo/index.js
+++ src/page/Selfvideo/index.js
@@ -1,15 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Player from 'griffith'
+import { Input, Row, Col } from "antd";
 
+const { Search } = Input;
 const Selfvideo = () => {
-  const sources = {
-    hd: {
-      play_url: 'http://localhost:3002/video.mp4',
-    }
+  const [src, setSrc] = useState('')
+  const onSearch = (value) => {
+    setSrc(value)
   }
+
   return (
     <div className="Selfvideo">
-      <Player sources={sources} standalone autoplay useMSE />
+      <Search
+        placeholder="请输入播放地址"
+        allowClear
+        enterButton="切换"
+        onSearch={(value) => {
+          onSearch(value)
+        }}
+      />
+      <Row gutter={16}>
+        <Col span={18} offset={3}>
+          {src && (
+            <Player
+              id={src}
+              sources={{ hd: { play_url: src } }}
+              cover=''
+              duration={1}
+              standalone
+              autoplay
+              useMSE
+              disablePictureInPicture
+            />
+          )}
+        </Col>
+      </Row>
+
     </div>
   );
 };
