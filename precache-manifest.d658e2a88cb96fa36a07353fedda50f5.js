f6d0da7da948eb0ea142907165c47a0fc5cedfd1
trlgml <trlgml@users.noreply.github.com>
2022-03-18 09:19:35 +0000

    deploy: 7f2529a626df8903fcff335ac9a90de5dceede0b

---
+++ precache-manifest.d658e2a88cb96fa36a07353fedda50f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f43360706f501e46ec4809b87f366fb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "576753d814625e36d483",
+    "url": "./static/css/2.5d8facc6.chunk.css"
+  },
+  {
+    "revision": "45de6a91d0109057af20",
+    "url": "./static/css/main.14d56211.chunk.css"
+  },
+  {
+    "revision": "576753d814625e36d483",
+    "url": "./static/js/2.476bd720.chunk.js"
+  },
+  {
+    "revision": "622cdbdd25fc1c09a3deec97ac4c5384",
+    "url": "./static/js/2.476bd720.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45de6a91d0109057af20",
+    "url": "./static/js/main.f4d81233.chunk.js"
+  },
+  {
+    "revision": "d0c16c0cdd01bf44169f",
+    "url": "./static/js/runtime-main.8d48727e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.eae32932ed617d1007a5529f3c44c8dc.js"
+  "./precache-manifest.d658e2a88cb96fa36a07353fedda50f5.js"
 );
 
 self.addEventListener('message', (event) => {
