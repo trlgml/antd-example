81bfebe1ba03f6cb9114b5929a72a5fcfeb9ef35
trlgml <1229771221@qq.com>
2022-06-24 16:14:04 +0800

    feat:test style components

--- src/page/Test/index.js
+++ src/page/Test/index.js
@@ -1,20 +1,59 @@
 import React from 'react'
-import { connect } from "react-redux";
+
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  :root {
+    --bg-color:red
+  }
+  .Test {
+    background-color: var(--bg-color);
+    height: 100vh;
+  }
+`;
+
+export const GlobalTeslaEngineStyle = createGlobalStyle`
+  :root {
+    --bg-color:blue
+  }
+  #root {
+    font-size: 22px;
+    margin-top: 140px;
+    font-family: 'Universal Sans', 'Hiragino Sans';
+  }
+
+  @media (prefers-color-scheme: dark) {
+    :root {
+      --bg-color:green
+    }
+    #root {
+      margin-top: 540px;
+    }
+  }
+`;
+
+const isTeslaEngine = () => {
+  const { userAgent } = window.navigator;
+  if (/MicroMessenger/i.test(userAgent) && /TESLA_/i.test(userAgent)) {
+    return true;
+  }
+  return false;
+};
 
 const Test = (props) => {
-
+  const ua = window.navigator.userAgent
+  
   return (
     <div className="Test">
-
+      你好，hello
+      <br/>
+      {ua}
+      <GlobalStyle />
+      {isTeslaEngine() && <GlobalTeslaEngineStyle/>}
     </div>
   );
 };
 
-const mapStateToProps = () => {
 
-};
 
-const mapDispatchToProps = {
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Test);
+export default Test;
